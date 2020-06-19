5950c1b4911963a950b317460854376977754bee
Yee-Q <1051195623@qq.com>
2020-06-19 08:49:09 +0800

    Please enter the commit message for your changes. Lines starting

--- src/main/java/com/yeexang/community/mapper/UserMapper.java
+++ src/main/java/com/yeexang/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.yeexang.community.mapper;
 import com.yeexang.community.pojo.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 @Mapper
@@ -12,6 +14,9 @@ public interface UserMapper {
     @Insert("INSERT INTO user (name, account_id, token, gmt_create, gmt_modified) " +
             "VALUES (#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insert(User user);
+
+    @Select("SELECT * FROM user WHERE token = #{token}")
+    User findByToken(@Param("token") String token);
 
 
 }
